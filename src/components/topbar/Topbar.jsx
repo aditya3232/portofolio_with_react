9beb54d8f5192d35cd15d96bfed43b1e3f8947c2
adit <m.aditya3232@gmail.com>
2021-11-21 15:12:25 +0700

    React sass global variables, install material-ui icons, & styling topbar
    - membuat global.scss yg berisi variabel scss, yg bisa diimport oleh scss lain
    - menambahkan id untuk setiap components, agar dapat dipanggil dengan href menggunakan "#id"
    - install material icon: npm add @material-ui/core  &  npm add @material-ui/icons
    - jika compiling lama, tunggu aja
    - dengan menggunakan material-ui icon akan dapat berubah warna, & tidak pecah jika resize
    - styling topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+82 811 3600 816</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>aditya@genius.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
