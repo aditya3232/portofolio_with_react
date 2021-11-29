6e81b25d36d47f14327da46223b5df8bc1ce415a
adit <m.aditya3232@gmail.com>
2021-11-29 14:13:20 +0700

    React introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,22 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+                
+            <div className="right">
+                <div className="wrappper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Muhammad Aditya</h1>
+                    <h3>Freelance <span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
