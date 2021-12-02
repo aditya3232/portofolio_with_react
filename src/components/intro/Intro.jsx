e396c990bd12ae63b27f37938d4e134228c2e117
adit <m.aditya3232@gmail.com>
2021-12-02 11:42:39 +0700

    React text typing animation
    - memberikan animasi text typing dengan "ityped" npm install ityped
    - beserta custom ityped nya

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,23 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+    // useRef fungsinya seperti document.querySelector() [memilih elemen]
+    const textRef = useRef();
+
+    // ityped
+    // useEffect akan menerapkan animasi ityped
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Designer", "Content Creator"],
+        });
+    },[]);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -13,7 +30,8 @@ export default function Intro() {
                 <div className="wrappper">
                     <h2>Hi There, I'm</h2>
                     <h1>Muhammad Aditya</h1>
-                    <h3>Freelance <span></span></h3>
+                    {/* elemen h3 menerapkan animasi ityped */}
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
