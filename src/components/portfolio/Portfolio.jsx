bc517268313d0002706bf89ab8cc2067b69004b0
adit <m.aditya3232@gmail.com>
2021-12-08 10:44:45 +0700

    membuat data dinamis dengan useEffect hook
    - ceritanya memassing data dari data.js ke halaman portfolio.jsx
    - sama seperti passing data dari controller ke view

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio,
+} from "../../data";
+
 
 export default function Portfolio() {
 
     // jiks list diselect akan aktif.
     const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
 
     // list (li) untuk (ul) Portfolio
     const list = [
@@ -30,6 +39,32 @@ export default function Portfolio() {
             title: "Content",
         },
     ];
+    
+    // menggunakan efek yg akan setData ketika diklik
+    useEffect(()=>{
+
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "content":
+                setData(contentPortfolio);
+                break;
+            default: 
+                setData(featuredPortfolio);    
+        }
+
+    },[selected])
+
 
     return (
         <div className="portfolio" id="portfolio">
@@ -40,32 +75,21 @@ export default function Portfolio() {
                     title={item.title} 
                     active={selected === item.id} 
                     setSelected={setSelected} 
-                    id={item}
+                    id={item.id}
                     />
                 ))}
             </ul>
             {/* container portfolio items */}
             <div className="container">
+                {/* foreach data */}
+                {data.map((d)=> (
+
                 <div className="item">
-                    <img src="" alt="" />
-                    <h3>Banking App</h3>
+                    <img src={d.img} alt="" />
+                    <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Banking App</h3>
-                </div>
+
+                ))}
             </div>
         </div>
     )
